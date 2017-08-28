b4f5d5c051d5edbed40bd4c33a1de8152abf0f24
Tsui YuenHong <625927650@qq.com>
2017-08-28 19:08:16 +0800

    init project

---
+++ ASS_Demo/ASS_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASS_Demo
+//
+//  Created by TsuiYuenHong on 2017/8/28.
+//  Copyright © 2017年 TsuiYuenHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASS_Demo/ASS_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASS_Demo
+//
+//  Created by TsuiYuenHong on 2017/8/28.
+//  Copyright © 2017年 TsuiYuenHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
